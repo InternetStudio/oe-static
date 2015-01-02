4c0c0bd371d733b37a4d40c3fcec786903a66bfe
alexharris <alexharris6@gmail.com>
2015-01-01 19:28:13 -0800

    css changes for refills for schedule page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -39,9 +39,41 @@ function addPlaceholder(target, text) {
 	$(target).attr('placeholder', text);
 }
 
-$(document).ready(function(){
-	back2Top();
-	toggleMenu();
-	stupidLinkBorderImageThing();
-	addPlaceholder('#mc_mv_EMAIL', 'Email');
-});
+function refillsVerticalAccordion(){
+
+	$('.js-vertical-tab-content').hide();
+	$('.js-vertical-tab-content:first').show();
+
+	/* if in tab mode */
+	$('.js-vertical-tab').click(function(event) {
+		event.preventDefault();
+
+		console.log('hello');
+
+		$('.js-vertical-tab-content').hide();
+		var activeTab = $(this).attr('rel');
+		$('#'+activeTab).show();
+
+		$('.js-vertical-tab').removeClass('is-active');
+		$(this).addClass('is-active');
+
+		$('.js-vertical-tab-accordion-heading').removeClass('is-active');
+		$('.js-vertical-tab-accordion-heading[rel^="'+activeTab+'"]').addClass('is-active');
+	});
+
+	/* if in accordion mode */
+	$('.js-vertical-tab-accordion-heading').click(function(event) {
+		event.preventDefault();
+
+		$('.js-vertical-tab-content').hide();
+		var accordionActiveTab = $(this).attr('rel');
+		$('#'+accordionActiveTab).show();
+
+		$('.js-vertical-tab-accordion-heading').removeClass('is-active');
+		$(this).addClass('is-active');
+
+		$('.js-vertical-tab').removeClass('is-active');
+		$('.js-vertical-tab[rel^="'+accordionActiveTab+'"]').addClass('is-active');
+	});
+}
+
